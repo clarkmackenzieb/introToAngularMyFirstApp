740d7b1914abe9e46340430742839799709a8c07
Mackenzie <clarkmackenzieb@gmail.com>
2017-09-04 16:04:05 -0500

    ??? anuaglear???

--- todos/myCtrl.js
+++ todos/myCtrl.js
@@ -1,1 +1,36 @@
  
+
+angular.module("todos")
+.controller("todoCtrl", function($scope){
+
+    $scope.todos = [ 
+        "create a todo list", 
+        "update this",
+        "remove things from list",
+        "show completed stuff"
+    ];
+
+    $scope.addTodo = function(){
+        $scope.todos.push($scope.newTodo);
+        $scope.newTodo = "";
+    }
+
+    $scope.removeTodo = function(todo){
+        $scope.todos = $scope.todos.filter(function(cur){
+            return cur !== todo;
+        })
+        
+    }
+
+    $scope.startEdit = function(todo, index){
+        $scope.editedTodo = todo;
+        $scope.index = index;
+        $scope.editing = true;
+    }
+
+    $scope.modifyTodo = function(todo){
+        $scope.todos[$scope.index] = todo;
+        $scope.editing = false;
+    }
+
+});
